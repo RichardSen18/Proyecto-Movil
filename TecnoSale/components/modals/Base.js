d61689175f6d590a90a913d7b55288137554434f
RichardSen18 <ricardosinecio@gmail.com>
2024-11-17 11:47:28 -0600

    modificaciones

--- TecnoSale/components/modals/Base.js
+++ TecnoSale/components/modals/Base.js
@@ -7,6 +7,7 @@ import {
   Dimensions,
 } from "react-native";
 import Colors from "../../constants/Colors";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const { height, width } = Dimensions.get("window");
 
@@ -21,18 +22,25 @@ export default function Base({ id, title, visible, children, onClose }) {
     >
       <TouchableOpacity style={styles.overlay} onPress={onClose} />
       <View style={styles.centeredView}>
+        
         <View style={styles.modalView}>
+          <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+            <Icon name="close" size={24} color={Colors.black} />
+          </TouchableOpacity>
+
           {title && (
-            <View>
-              <Text>{title}</Text>
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>{title}</Text>
             </View>
           )}
+          
           <View>{children}</View>
         </View>
       </View>
     </Modal>
   );
 }
+
 const styles = StyleSheet.create({
   centeredView: {
     flex: 1,
@@ -46,13 +54,27 @@ const styles = StyleSheet.create({
     paddingBottom: 15,
     paddingHorizontal: 15,
     paddingTop: 15,
+    position: "relative",
   },
   overlay: {
-    backgroundColor: "rgba(0,0,0,0,6)",
+    backgroundColor: "rgba(0,0,0,0.6)",
     height,
     left: 0,
     position: "absolute",
     top: 0,
     width,
+  },
+  closeButton: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+    zIndex: 1,
+  },
+  titleContainer: {
+    marginBottom: 10,
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
--- TecnoSale/screens/Home.js
+++ TecnoSale/screens/Home.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
-import { FlatList, ScrollView } from "react-native";
+import { useState } from 'react';
+import { FlatList, ScrollView } from 'react-native';
 
-import { Content, Header, Wrapper } from "../components/layout";
-import State from "../components/controls/State";
-import Base from "../components/modals/Base";
-import FormItem from "../components/controls/Form_item";
-import Button from "../components/controls/Button";
+import { Content, Header, Wrapper } from '../components/layout';
+import State from '../components/controls/State';
+import Base from '../components/modals/Base';
+import FormItem from '../components/controls/Form_item';
+import Button from '../components/controls/Button';
 
 export default function Home({ navigation }) {
   const [visible, setVisible] = useState(false);
   const [selected, setSelected] = useState();
   const data = [
-    { id: 1, name: "Querétaro", code: "QRO", status: true },
-    { id: 2, name: "Guerrero", code: "GUE", status: true },
-    { id: 3, name: "Guanajuato", code: "GUA", status: false },
+    { id: 1, name: 'Querétaro', code: 'QRO', status: true },
+    { id: 2, name: 'Guerrero', code: 'GUE', status: true },
+    { id: 3, name: 'Guanajuato', code: 'GUA', status: false },
   ];
 
   const toggleModal = () => {
@@ -23,6 +23,7 @@ export default function Home({ navigation }) {
   return (
     <Wrapper>
       <Header title="Dashboard" />
+
       <Content>
         {visible && (
           <Base
@@ -36,15 +37,17 @@ export default function Home({ navigation }) {
             <FormItem label="Estatus" />
           </Base>
         )}
+
         <Button label="Abrir modal" onPress={toggleModal} />
+        
         <ScrollView horizontal={true}>
           <FlatList
             data={data}
             renderItem={State}
-            keyExtractor={(item) => item.id}
+            keyExtractor={item => item.id}
           />
         </ScrollView>
       </Content>
     </Wrapper>
   );
-}
+};
